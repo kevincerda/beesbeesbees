f5f782cedd8a0fc44e018d25e6fb96c5087e9130
Kevin <kevin@cerda.io>
2018-04-30 12:01:12 -0700

    Completed beesbeesbees.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,11 @@
 var ForagerBee = function() {
+  HoneyMakerBee.call(this);
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+ForagerBee.prototype = Object.create(Grub.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(item) {this.treasureChest.push(item)};
 
